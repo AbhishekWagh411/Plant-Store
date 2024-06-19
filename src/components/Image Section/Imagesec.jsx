fc19e4c09e2bb3e2c1f80cae12515a9c8d6d3c7f
AbhishekWagh411 <abhishekwagh412@gmail.com>
2024-06-19 21:09:30 +0530

    added image and black bg

--- src/components/Image Section/Imagesec.jsx
+++ src/components/Image Section/Imagesec.jsx
@@ -1,25 +1,32 @@
 import React from 'react'
 import './Imagesec.css';
+import tree1 from '../../Assets/tree1.png';
 export default function Imagesec() {
     return (
-        <div className="background">
-          <div className='left-side'>
-          <span className='text' > Buy your dream plants</span>
-          <div className='belowtext'>
-            <div className='textWrapper'>  
-              <span className='text50' >50+</span>
-              <span className='textin50'>Plant Species</span>
+            <div className="background">
+              <div className='left-side'>
+                <span className='text' > Buy your dream plants</span>
+                  <div className='belowtext'>
+                    <div className='textWrapper'>  
+                      <span className='text50' >50+</span>
+                      <span className='textin50'>Plant Species</span>
+                    </div>
+                    <div className='pipe'>|</div>
+                    <div className='textWrapper'>
+                      <span className='text100'>100+</span>
+                      <span className='textin100'>Customers</span>
+                    </div>
+                  </div>
+              </div>
+              <div className='right-side'>
+                <div className='blackSpot-container'>
+                  <div className='blackSpot'>
+                  </div>
+                  <div className='treeImage'>
+                       <img src={tree1} className='tree' />
+                    </div> 
+                </div>  
+              </div>
             </div>
-            <div className='pipe'>|</div>
-            <div className='textWrapper'>
-              <span className='text100'>100+</span>
-              <span className='textin100'>Customers</span>
-            </div>
-          </div>
-        </div>
-        <div className='right-side'>
-
-          </div>
-        </div>
-      );
-}
+          );
+  }
